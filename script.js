3574fa73f286cadba6fe23f26df3f07b70d33d25
Karin Sundin <88103504+sundinkarin@users.noreply.github.com>
2022-12-20 13:35:04 -0500

    questions

--- script.js
+++ script.js
@@ -2,23 +2,56 @@ var questionBox = document.querySelector("#question")
 
 
 
-var questions = [{
-    question: "What are the primitive data types?",
-    answers: ["Number, string, Boolean, undefined.", 15, 3, 8],
-    correctAnswer: "Number, string, Boolean, undefined."
-},
-{
-    question: "What is a variable?",
-    answers: [5, 15, 3, "A place to store data that will change."],
-    correctAnswer: "A place to store data that will change."
-},
-{
-    question: "What is a mouse event?",
-    answers: [5, 15, "Mousedown, mouseover, mouseout.", 8],
-    correctAnswer: "Mousedown, mouseover, mouseout."
-},
+var questions = [
+    {
+        question: "Which is not a primitive data type?",
+        answers: ["number", "string", "Boolean", "object"],
+        correctAnswer: "object"
+    },
+    {
+        question: "How do you define a variable?",
+        answers: ["var", "=", "true", "div"],
+        correctAnswer: "var"
+    },
+    {
+        question: "What is not a mouse event?",
+        answers: ["mousedown", "multiple", "mouseover", "mouseout"],
+        correctAnswer: "multiple"
+    },
+    {
+        question: "How do you define an object?",
+        answers: ["()", "<>", "{}", "[]"],
+        correctAnswer: "[]"
+    },
+    {
+        question: "Why are arrays used?",
+        answers: ["data storage", "unrelated objects", "for complexity", "mouseout"],
+        correctAnswer: "data storage"
+    },
+    {
+        question: "How are loops used?",
+        answers: ["Loops check a condition.", "runs only once", "not recommended", "logical operator"],
+        correctAnswer: "Loops check a condition."
+    },
+    {
+        question: "What lets you group a series of statements to perform a specific task?",
+        answers: ["loops", "functions", "object", "statement"],
+        correctAnswer: "functions"
+    },
+    {
+        question: "What is another name for Local and Global Variables?",
+        answers: ["data", "loops", "arrays", "scope"],
+        correctAnswer: "scope"
+    },
 ]
 console.log(questions[1].question)
-questionBox.innerHTML = questions[1].question
+// questionBox.innerHTML = questions[1].question
 
-// make a variable for the start button. Then we need to add an event listener to this button. Write a function that this add event listener calls.
+
+
+// make a variable for the start button. 
+// Then we need to add an event listener to this button. 
+// Write a function that this add event listener calls.
+
+var startButton = "start";
+function keydownAction()
